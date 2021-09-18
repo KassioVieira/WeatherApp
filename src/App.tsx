e03adbe7cd1dafa17720d61d7af40aa29f46f903
KassioVieira <kassio.vieira7@gmail.com>
2021-09-18 17:54:01 -0300

    flipper removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safe}>
       <View style={styles.container}>
         <Text>Weather App</Text>
       </View>
@@ -12,6 +12,10 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+  },
+
   container: {
     flex: 1,
     justifyContent: 'center',
