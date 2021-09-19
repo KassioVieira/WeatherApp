93b4286c2cb6f89943f68b8b4de44858affa1d35
KassioVieira <kassio.vieira7@gmail.com>
2021-09-19 09:16:01 -0300

    autocomplete component added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+
 import colors from 'values/colors';
 import {NavBarProps} from './NavBar.types';
 
-import {Header, Title, Input} from './NavBar.styles';
+import {Header, Title} from './NavBar.styles';
+
+interface CityProps {
+  description: string;
+}
 
 const NavBar = ({title}: NavBarProps) => {
   const [isOpen, setOpen] = useState(false);
@@ -13,13 +19,35 @@ const NavBar = ({title}: NavBarProps) => {
     setOpen(!isOpen);
   };
 
+  const add = (city: CityProps) => {
+    console.tron.log(city.description);
+  };
+
   const renderSearhcInput = () => {
     return (
       <>
         <TouchableOpacity onPress={handleOnPress}>
           <Icon name="close" size={30} color={colors.secondary} />
         </TouchableOpacity>
-        <Input autoFocus={true} />
+        {/* <Input autoFocus={true} /> */}
+        <GooglePlacesAutocomplete
+          placeholder=""
+          onPress={(data: CityProps) => {
+            add(data);
+          }}
+          listViewDisplayed={false}
+          query={{
+            key: 'AIzaSyAAIXdgMWTWmPKqPds5FZPMAaFc9C7LJBU',
+            language: 'pt_BR',
+          }}
+          styles={{
+            listView: {
+              zIndex: 9,
+              position: 'absolute',
+              top: 51,
+            },
+          }}
+        />
       </>
     );
   };
